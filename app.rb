db68b1d7fb330563ef74c9649937171e51ddcb67
kevin lang <langkevina@gmail.com>
2019-09-01 20:06:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,11 +1,10 @@
 require './environment'
 
-module FormsLab
+
   class App < Sinatra::Base
 
     # code other routes/actions here
     get '/' do
-<<<<<<< HEAD
       erb :root
     end
     get '/new' do
@@ -37,7 +36,6 @@ module FormsLab
       
       erb :'pirates/show'
       # binding.pry
->>>>>>> e17a3cd19f0a01cae31c68a1efe07a077438c990
+
     end
   end
-end
