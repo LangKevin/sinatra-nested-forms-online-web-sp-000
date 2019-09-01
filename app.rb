0154d17ab552299d9861ef6f2b29d8185c0b3a7e
kevin lang <langkevina@gmail.com>
2019-09-01 20:05:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ module FormsLab
 
     # code other routes/actions here
     get '/' do
+<<<<<<< HEAD
       erb :root
     end
     get '/new' do
@@ -18,6 +19,25 @@ module FormsLab
       @ships = Ship.all
 
       erb :'pirates/show'
+=======
+       erb :root
+    end
+    get '/new' do
+       erb :'pirates/new'
+    end
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+
+        Ship.new(details)
+      end
+     
+      @ships = Ship.all
+      
+      erb :'pirates/show'
+      # binding.pry
+>>>>>>> e17a3cd19f0a01cae31c68a1efe07a077438c990
     end
   end
 end
